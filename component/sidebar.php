d19e71bb49fde9baf0fc042b9c406825789064ee
ibrahim569822 <ibrahim569822@gmail.com>
2026-07-28 09:26:57 +0600

    getting updates

--- component/sidebar.php
+++ component/sidebar.php
@@ -3,26 +3,26 @@
         <div class="sidebar">
                 <!-- Sidebar -->
                <div class="sidebar-header">
-                   <div class="lg-logo"><a href="index.html"><img src="./assets/images/logo.png" alt="logo large"></a></div>
-                   <div class="sm-logo"><a href="index.html"><img src="./assets/images/small-logo.png" alt="logo small"></a></div>
+                   <div class="lg-logo"><a href="index.html"><img src="/assets/images/logo.png" alt="logo large"></a></div>
+                   <div class="sm-logo"><a href="index.html"><img src="/assets/images/small-logo.png" alt="logo small"></a></div>
                </div>
                <div class="sidebar-body  custom-scrollbar">
                     <ul class="sidebar-menu">
-                        <li><a href="index.html" class=" sidebar-link active"><i class="fa-solid fa-house"></i><p>Dashboard</p></a></li>
-                        <li><a href="course.html" class="sidebar-link"><i class="fa-brands fa-discourse"></i><p>Courses</p></a></li>
-                        <li><a href="students.html" class=" sidebar-link"><i class="fa-solid fa-user"></i><p>Students</p></a></li>
-                        <li><a href="teacher.html" class=" sidebar-link"><i class="fa-solid fa-chalkboard-user"></i><p>Teachers</p></a></li>
-                        <li><a href="library.html" class=" sidebar-link"><i class="fa-solid fa-book"></i><p>Library</p></a></li>
-                        <li><a href="department.html" class=" sidebar-link"><i class="fa-solid fa-building"></i><p>Department</p></a></li>
-                        <li><a href="staff.html" class="sidebar-link"><i class="fa-solid fa-users"></i><p>Staff</p></a></li>
-                        <li><a href="fees.html" class="sidebar-link"><i class="fa-solid fa-dollar-sign"></i><p>Fees</p></a></li>
+                        <li><a href="<?php echo $base_url; ?>dashboard.php" class=" sidebar-link active"><i class="fa-solid fa-house"></i><p>Dashboard</p></a></li>
+                        <li><a href="<?php echo $base_url; ?>course/courselist.php" class="sidebar-link"><i class="fa-brands fa-discourse"></i><p>Courses</p></a></li>
+                        <li><a href="<?php echo $base_url; ?>trainees/list.php" class=" sidebar-link"><i class="fa-solid fa-user"></i><p>Students</p></a></li>
+                        <li><a href="<?php echo $base_url; ?>teacher.php" class=" sidebar-link"><i class="fa-solid fa-chalkboard-user"></i><p>Teachers</p></a></li>
+                        <li><a href="<?php echo $base_url; ?>library.php" class=" sidebar-link"><i class="fa-solid fa-book"></i><p>Library</p></a></li>
+                        <li><a href="<?php echo $base_url; ?>department.php" class=" sidebar-link"><i class="fa-solid fa-building"></i><p>Department</p></a></li>
+                        <li><a href="<?php echo $base_url; ?>staff.php" class="sidebar-link"><i class="fa-solid fa-users"></i><p>Staff</p></a></li>
+                        <li><a href="<?php echo $base_url; ?>fees.php" class="sidebar-link"><i class="fa-solid fa-dollar-sign"></i><p>Fees</p></a></li>
                         <li><a href="#" class=" sidebar-link submenu-parent"><i class="fa-solid fa-list"></i><p>Pages <i class="fa-solid fa-chevron-right right-icon"></i></p></a>
                             <ul class="sidebar-submenu">
-                                <li><a href="login.html" class="submenu-link"><i class="fa-solid fa-circle me-4 font-size-12"></i><p class="m-0">Login</p></a></li>
-                                <li><a href="signup.html" class="submenu-link"><i class="fa-solid fa-circle me-4 font-size-12"></i><p class="m-0">Register</p></a></li>
-                                <li><a href="forgot-password.html" class="submenu-link"><i class="fa-solid fa-circle me-4 font-size-12"></i><p class="m-0">Forgot password</p></a></li>
-                                <li><a href="404.html" class="submenu-link"><i class="fa-solid fa-circle me-4 font-size-12"></i><p class="m-0">404 page</p></a></li>
-                                <li><a href="500.html" class="submenu-link"><i class="fa-solid fa-circle me-4 font-size-12"></i><p class="m-0">500 page</p></a></li>
+                                <li><a href="<?php echo $base_url; ?>login.php" class="submenu-link"><i class="fa-solid fa-circle me-4 font-size-12"></i><p class="m-0">Login</p></a></li>
+                                <li><a href="<?php echo $base_url; ?>signup.php" class="submenu-link"><i class="fa-solid fa-circle me-4 font-size-12"></i><p class="m-0">Register</p></a></li>
+                                <li><a href="<?php echo $base_url; ?>forgot-password.php" class="submenu-link"><i class="fa-solid fa-circle me-4 font-size-12"></i><p class="m-0">Forgot password</p></a></li>
+                                <li><a href="<?php echo $base_url; ?>404.php" class="submenu-link"><i class="fa-solid fa-circle me-4 font-size-12"></i><p class="m-0">404 page</p></a></li>
+                                <li><a href="<?php echo $base_url; ?>500.php" class="submenu-link"><i class="fa-solid fa-circle me-4 font-size-12"></i><p class="m-0">500 page</p></a></li>
                             </ul>
                         </li>
                         <li><a href="#" class=" sidebar-link submenu-parent"><i class="fa-solid fa-list"></i><p>Table <i class="fa-solid fa-chevron-right right-icon"></i></p></a>
