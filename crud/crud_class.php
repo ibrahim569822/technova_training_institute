80eabfe4339c1a19f49a52c8722c14506e3d021e
ibrahim569822 <ibrahim569822@gmail.com>
2026-08-03 12:13:34 +0600

    updates sign up

--- crud/crud_class.php
+++ crud/crud_class.php
@@ -108,7 +108,7 @@ class crud_class{
 
             // "SELECT * FROM users WHERE id='1' AND name='kamal' ORDER BY name ASC LIMIT 10 OFFSET 5"
         }
-
+echo $sql;
         $rs = $this->conn->query($sql);
 
         if($rs->num_rows > 0){
