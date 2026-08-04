f10717925670bf76ba7635e5024ccc86c8cd3acd
Akibul Islam <akib34108@gmail.com>
2026-08-04 10:13:44 +0600

    category

--- courses/categories/create.php
+++ courses/categories/create.php
@@ -18,11 +18,17 @@
     <div class="mt-4">
       <div class="card shadow-sm border-0">
         <div class="card-body p-0">
-          <form action="<?= $base_url; ?>categories/store.php" method="POST" enctype="multipart/form-data" class="p-4">
+          <form action="<?= $base_url; ?>courses/categories/store.php" method="POST" enctype="multipart/form-data" class="p-4">
             <div class="row">
               <div class="col-md-12 mb-3">
                 <label for="category_name" class="form-label">Category Name</label>
                 <input type="text" class="form-control" id="category_name" name="category_name" required>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-12 mb-3">
+                <label for="category_description" class="form-label">Category Description</label>
+                <input type="text" class="form-control" id="category_description" name="category_description" required>
               </div>
             </div>
             <div class="row">
--- courses/categories/store.php
+++ courses/categories/store.php
@@ -1,30 +1,13 @@
 <?php
-    require_once "../component/connection.php";
+    require_once "../../component/connection.php";
 
-        $_POST['password'] = sha1($_POST['password']);
 
-        // Handle file upload
-        if (isset($_FILES['image']) && $_FILES['image']['error'] === UPLOAD_ERR_OK) {
-            $uploadDir = '../assets/uploads/trainees/images/';
-            $imageName = rand(1, 999999). time() . '_' . basename($_FILES['image']['name']);
-            $uploadFile = $uploadDir . $imageName ;
-
-            if (move_uploaded_file($_FILES['image']['tmp_name'], $uploadFile)) {
-                $_POST['image'] = $imageName;
-            } else {
-                echo "Error uploading file.";
-                exit;
-            }
-        } else {
-            $_POST['image'] = null; // No file uploaded
-        }
-
-        $result = $crud->common_insert("trainees", $_POST);
+        $result = $crud->common_insert("categories", $_POST);
         if ($result['status']) {
             $_SESSION['message'] = array('success','Success', $result['message']);
         } else {
             $_SESSION['message'] = array('danger','Error', $result['message']);
         }
 
-        echo "<script>window.location.href = '".$base_url."trainees/list.php';</script>";
+        echo "<script>window.location.href = '".$base_url."courses/categories/list.php';</script>";
     
