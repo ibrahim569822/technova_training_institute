e3bec86e15d2a26d311ea39fa072dee9b308ed46
Akibul Islam <akib34108@gmail.com>
2026-08-04 09:04:36 +0600

    changes

--- teacher/delete.php
+++ teacher/delete.php
@@ -1,12 +1,11 @@
 <?php
-    require_once "../component/connection.php";
+require_once "../component/connection.php";
 
-    $result = $crud->common_update("trainees", ['deleted_at' => date('Y-m-d H:i:s')], ['id' => $_GET['id']]);
-    if ($result['status']) {
-        $_SESSION['message'] = array('success','Success', $result['message']);
-    } else {
-        $_SESSION['message'] = array('danger','Error', $result['message']);
-    }
+$result = $crud->common_update("teacher", ['deleted_at' => date('Y-m-d H:i:s')], ['id' => $_GET['id']]);
+if ($result['status']) {
+    $_SESSION['message'] = array('success', 'Success', $result['message']);
+} else {
+    $_SESSION['message'] = array('danger', 'Error', $result['message']);
+}
 
-    echo "<script>window.location.href = '".$base_url."trainees/list.php';</script>";
-    
+echo "<script>window.location.href = '" . $base_url . "teacher/list.php';</script>";
