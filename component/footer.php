fa1c22068634fb287a5c18780e87fe26bc48deaf
Akibul Islam <akib34108@gmail.com>
2026-08-09 09:42:03 +0600

    update invoice

--- component/footer.php
+++ component/footer.php
@@ -1,5 +1,5 @@
 <!-- Footer -->
-             <div class="footer text-center bg-white shadow-sm py-3 mt-5">
+             <div class="footer text-center bg-white shadow-sm py-3 mt-5 no-print">
                 <p class="m-0">Copyright © 2024. All Rights Reserved. <a href="https://www.templaterise.com/" class="text-primary" target="_blank" >Themes By TemplateRise</a></p>
             </div>
     </div>
