cad0da7bfb223f022d3b796b83372e7925ef1048
Akibul Islam <akib34108@gmail.com>
2026-08-09 12:43:50 +0600

    added static fees page

--- component/footer.php
+++ component/footer.php
@@ -1,6 +1,6 @@
 <!-- Footer -->
              <div class="footer text-center bg-white shadow-sm py-3 mt-5 no-print">
-                <p class="m-0">Copyright © 2024. All Rights Reserved. <a href="https://www.templaterise.com/" class="text-primary" target="_blank" >Themes By TemplateRise</a></p>
+                <p class="m-0">Copyright © 2024. All Rights Reserved. <a href="<?= $base_url ?>" class="text-primary" target="_blank" >Technova Training Institute</a></p>
             </div>
     </div>
      <!-- Scripts -->
