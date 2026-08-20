421a184f212f8a73a2639d17195623865c0579d4
Akibul Islam <akib34108@gmail.com>
2026-08-20 11:05:16 +0600

    update

--- Certificates/update.php
+++ Certificates/update.php
@@ -1,48 +1,13 @@
 <?php
+require_once "../component/connection.php";
 
-require_once "../component/header.php";
-require_once "../component/sidebar.php";
 
-$id = $_POST['certificate_id'] ?? '';
-$certificate_no = $_POST['certificate_no'] ?? '';
-$issue_date = $_POST['issue_date'] ?? '';
-$status = $_POST['status'] ?? 0;
+$Cert_data['trainee_id'] = $_POST['trainee_id'];
+$Cert_data['course_id'] = $_POST['course_id'];
+$Cert_data['batch_id'] = $_POST['batch_id'];
+$Cert_data['certificate_no'] = $_POST['certificate_no'];
+$Cert_data['issue_date'] = $_POST['issue_date'];
+$Cert_data['status'] = $_POST['status'];
+$result = $crud->common_update("certificates", $Cert_data, ['certificate_id' => $_POST['certificate_id']]);
 
-if (
-    empty($id) ||
-    empty($certificate_no) ||
-    empty($issue_date)
-) {
-    die("Required fields are missing.");
-}
-
-$certificate_no = $crud->conn->real_escape_string($certificate_no);
-$issue_date = $crud->conn->real_escape_string($issue_date);
-$status = (int) $status;
-$id = (int) $id;
-
-$query = "
-    UPDATE certificates
-    SET
-        certificate_no = '$certificate_no',
-        issue_date = '$issue_date',
-        status = '$status'
-    WHERE certificate_id = '$id'
-";
-
-$result = $crud->common_query($query);
-
-if ($result['status']) {
-
-    echo "<script>
-        window.location.href = 'index.php?updated=1';
-    </script>";
-
-    exit;
-}
-
-die("Certificate update failed.");
-
-?>
-
-<?php require_once "../component/footer.php"; ?>
+echo "<script>window.location.href = '" . $base_url . "certificates/list.php';</script>";
