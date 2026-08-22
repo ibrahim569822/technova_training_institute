7832a14437faa64e32c95c95e5e091a70c2cdd47
Akibul Islam <akib34108@gmail.com>
2026-08-22 11:06:47 +0600

    update

--- component/header.php
+++ component/header.php
@@ -1,5 +1,15 @@
 <?php
-    require_once $_SERVER['DOCUMENT_ROOT'] . "/technova_training_institute/component/connection.php";
+
+    // Define a list of known local environments
+    $local_hosts = ['localhost', '127.0.0.1', '::1'];
+
+    if (in_array($_SERVER['HTTP_HOST'], $local_hosts) || in_array($_SERVER['REMOTE_ADDR'], $local_hosts)) {
+        require_once $_SERVER['DOCUMENT_ROOT'] . "/technova_training_institute/component/connection.php";
+    } else {
+        require_once $_SERVER['DOCUMENT_ROOT'] . "/component/connection.php";
+    }
+
+    
     if(!isset($_SESSION['is_logged_in']) || !$_SESSION['is_logged_in']){
         echo "<script>window.location='{$base_url}login.php'</script>";
         exit;
